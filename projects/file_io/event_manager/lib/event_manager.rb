1a18bb580d78c8d75e7a31b68c9a65660374b312
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2022-11-04 13:01:26 -0700

    Google API for email done

--- projects/file_io/event_manager/lib/event_manager.rb
+++ projects/file_io/event_manager/lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pry-byebug'
 
 class EventManager
 
@@ -73,11 +74,20 @@ class EventManager
     end
 
     def clean_zip(zip)
-        zip = "00000" if zip == nil or zip == ""
-        zip.rjust(5, '0') if zip.length < 5
-        zip[0..4] if zip.length > 5
+        # zip = "00000" if zip == nil or zip == ""
+        if zip == nil
+            zip = "00000"
+        else
+            zip.chomp!
+            if zip.length > 5
+                zip[0..4] 
+            elsif zip.length < 5
+                while zip.length < 5 do
+                   zip = "0" + zip
+                end
+            end
+        end
         return zip
-
     end
 
     def print_column_from_lines(index=0)
@@ -113,6 +123,60 @@ class EventManager
             puts name
         end
     end
+
+    # GOOGLE CIVIC INFORMATION API
+    require 'csv'
+    require 'google/apis/civicinfo_v2'
+
+    def clean_zipcode(zipcode)
+        zipcode.to_s.rjust(5, '0')[0..4]
+    end
+
+    def google_api()
+
+        # Create Object and set API key
+        civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+        civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+        puts 'EventManager initialized.'
+
+        # Get the values from the CSV file. 
+        contents = CSV.open(
+            'event_attendees.csv',
+            headers: true, # CSV contains headers
+            header_converters: :symbol # Turn headers to sypbols
+        )
+
+        # For each "Person" in our row of the CSV
+        contents.each do |row|
+
+            # Get name and Cleaned zip code
+            name = row[:first_name]
+            zipcode = clean_zipcode(row[:zipcode])
+            
+            # Now use the zip code to find the legislators in that zip code
+            begin # This is like a try/catch block
+                legislators = civic_info.representative_info_by_address(
+                    address: zipcode,
+                    levels: 'country',
+                    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+                )
+                
+                # Get all of the officials listed under "officials query"
+                officials = legislators.officials
+                
+                # Turn the officials into a map to get the Key Value pairs such as name->patty
+                officials_names = officials.map do |official|
+                    puts "#{name} #{zipcode} #{official.name}"
+                end
+
+            rescue
+                'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+            end
+            # puts "#{name} #{zipcode} #{legislators}"
+        end
+    end
+
 end
 
 
@@ -120,13 +184,9 @@ end
 file_name = "event_attendees.csv" # Reads from whichever directory were on. If this file isnt int the current directory, then it needs to be specified
 em = EventManager.new(file_name)
 
-em.load_file_lines
-values = em.get_name_and_zips
-# puts values
-em.print_names_and_zips(values)
-# em.print_column_from_lines(2)
-#em.load_csv_file_and_print_row_using_token(:first_name)
+# em.load_file_lines
+# values = em.get_name_and_zips
+# em.print_names_and_zips(values)
 
-
-
-
+# Google API
+em.google_api
